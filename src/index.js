5722bef9dca8891d7e4d4fbe8a631255fb6cc3fd
Van Meer , Andrew <Andrew.VanMeer@londonlife.com>
2020-05-25 11:45:53 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const greeting =
+  <div>
+    <h1>Welcome to the Canada Life Interview</h1>
+    <p>If you are reading this message, you are setup and ready to begin. Instructions to follow.</p>
+  </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(greeting, document.getElementById('root'));
